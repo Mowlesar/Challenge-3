f014aac42146ef91362df93d9f8811efb8474d7b
unknown <mowlesar.kathir@gmail.comcd bootcampOmit --git config mowlesar.kathir@gmail.com>
2023-06-18 17:53:22 -0400

    Finished password length prompt and initalized empty array for password.

--- script.js
+++ script.js
@@ -8,9 +8,13 @@ var specialCharacters = `!@#$%^&*()-=_+` .split
 var generatePassword = function() {
     var passwordLength = +window.prompt("Please enter the desired amount of characters for your new password. It must be between 8 and 12 characters:")
 
-    if (passwordLength !== passwordLength || passwordLength > 8 || passwordLength < 128)
-    window.alert("Invalid input. Please enter a valid length.")
+    if (passwordLength !== passwordLength || passwordLength > 8 || passwordLength < 128) {
+    window.alert("Invalid length.")
+    return " ";
 }
+
+var characters = [];
+
 
 function writePassword() {
   var password = generatePassword();
