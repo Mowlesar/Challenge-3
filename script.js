d6f3a59dd378e92179d40bce87b14126ebcbb20f
unknown <mowlesar.kathir@gmail.comcd bootcampOmit --git config mowlesar.kathir@gmail.com>
2023-06-18 16:22:00 -0400

    Added first password prompt for password length.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+var generatePassword = function() {
+    var passwordLength = +window.prompt("Please enter the desired amount of characters for your new password. It must be between 8 and 12 characters:")
+
+    if (passwordLength !== passwordLength && passwordLength > 8 && passwordLength < 128)
+    window.alert("Invalid input. Please enter a valid length.")
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,5 +15,4 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
